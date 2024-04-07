d1b0940c987e59a00d4c4a678ace82e504756e67
Maksim Balashov <tracerb@inbox.ru>
2024-04-07 12:37:02 +0300

    modify TacoOrder.java and Taco.java and orderForm.html

--- src/main/java/com/balashovmaksim/taco/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/balashovmaksim/taco/tacocloud/controllers/DesignTacoController.java
@@ -3,9 +3,11 @@ package com.balashovmaksim.taco.tacocloud.controllers;
 import com.balashovmaksim.taco.tacocloud.model.Ingredient;
 import com.balashovmaksim.taco.tacocloud.model.Taco;
 import com.balashovmaksim.taco.tacocloud.model.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -50,7 +52,11 @@ public class DesignTacoController {
         return "design";
     }
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+    public String processTaco(@Valid Taco taco, Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder){
+        if (errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
